7a5c61ffdd60e27827bf1132fb65cd14d95b1811
Diego Castillo <diego.castilloguajardo@gmail.com>
2022-05-21 18:11:53 -0400

    remove Home.module import

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,5 @@
-import Head from "next/head"
-import Image from "next/image"
-import styles from "../styles/Home.module.css"
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -15,5 +14,5 @@ export default function Home() {
 
       <footer></footer>
     </div>
-  )
+  );
 }
